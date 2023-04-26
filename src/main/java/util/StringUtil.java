def1441996c95e5e62ab056b586ca34b0ce01bf0
songsunkook <songsunkook@gmail.com>
2023-04-27 02:35:37 +0900

    feat: StringUtil.split 메서드명 변경 및 기능 추가
    좌표의 앞, 뒤 (, )를 제거하는 문장 추가

--- src/main/java/util/StringUtil.java
+++ src/main/java/util/StringUtil.java
@@ -5,7 +5,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public abstract class StringUtil {
-	public static List<Integer> split(String input) {
+	private static final int STRING_TRIM_INDEX = 1;
+	public static List<Integer> process(String input) {
+		input = input.substring(STRING_TRIM_INDEX, input.length() - STRING_TRIM_INDEX);
 		List<String> list = Arrays.asList(input.split(","));
 
 		return list.stream()
--- src/test/java/util/StringUtilTest.java
+++ src/test/java/util/StringUtilTest.java
@@ -11,22 +11,22 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-@DisplayName("문자열을")
+@DisplayName("숫자에 대하여")
 public class StringUtilTest {
 
 	@ParameterizedTest
 	@DisplayName("정상적으로 자르는가")
-	@MethodSource("splitTestProvider")
-	public void splitTest(String string, List<Integer> result){
-		assertEquals(StringUtil.split(string), result);
+	@MethodSource("processTestProvider")
+	public void processTest(String string, List<Integer> result){
+		assertEquals(StringUtil.process(string), result);
 	}
 
 
-	private static Stream<Arguments> splitTestProvider() {
+	private static Stream<Arguments> processTestProvider() {
 		return Stream.of(
-			Arguments.of("1,2", Arrays.asList(1, 2)),
-			Arguments.of("24,1", Arrays.asList(24, 1)),
-			Arguments.of("13,22", Arrays.asList(13, 22))
+			Arguments.of("(1,2)", Arrays.asList(1, 2)),
+			Arguments.of("(24,1)", Arrays.asList(24, 1)),
+			Arguments.of("(13,22)", Arrays.asList(13, 22))
 		).map(arguments -> Arguments.of(arguments.get()[0], arguments.get()[1]));
 	}
 }
