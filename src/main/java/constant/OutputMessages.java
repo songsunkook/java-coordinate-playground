3c6cbfba86aacc58fc4c640458f6262750e27751
songsunkook <songsunkook@gmail.com>
2023-05-02 14:58:56 +0900

    refactor: 결과 출력문 enum 서식문자 변경

--- src/main/java/constant/OutputMessages.java
+++ src/main/java/constant/OutputMessages.java
@@ -4,7 +4,7 @@ public enum OutputMessages {
     INPUT_COORDINATES("좌표를 입력하세요."),
     OUTPUT_LINE("두 점 사이 거리는 %f"),
     OUTPUT_TRIANGLE("삼각형 넓이는 %.1f"),
-    OUTPUT_SQUARE("사각형 넓이는 %d");
+    OUTPUT_SQUARE("사각형 넓이는 %.0f");
 
     private final String message;
 
@@ -17,10 +17,6 @@ public enum OutputMessages {
     }
 
     public String getMessage(double input) {
-        return String.format(message, input);
-    }
-
-    public String getMessage(int input) {
         return String.format(message, input);
     }
 }
