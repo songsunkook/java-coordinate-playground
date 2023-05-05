9501d10cc0fca4856643d0218de2ad328aa02e9a
songsunkook <songsunkook@gmail.com>
2023-05-05 16:48:53 +0900

    fix: 삼각형 면적 구하기 로직 수정

--- src/main/java/service/TriangleCalculator.java
+++ src/main/java/service/TriangleCalculator.java
@@ -1,5 +1,6 @@
 package service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,11 +15,21 @@ public class TriangleCalculator extends CalculatorService {
     public double calculate() {
         double[] len = new double[3];
         for (int i = 0; i < 3; i++) {
-            List<Coordinate> coordinatesParam = coordinates.subList(0, 3);
+            List<Coordinate> coordinatesParam = setCoordinatesParam(i);
             LineCalculator lineCalculator = new LineCalculator(coordinatesParam);
             len[i] = Math.abs(lineCalculator.calculate());
         }
         double s = (Arrays.stream(len).sum()) / 2;
         return Math.sqrt(s * (s - len[0]) * (s - len[1]) * (s - len[2]));
+    }
+
+    private List<Coordinate> setCoordinatesParam(int index) {
+        if (index == 2) {
+            List<Coordinate> result = new ArrayList<>();
+            result.add(coordinates.get(0));
+            result.add(coordinates.get(2));
+            return result;
+        }
+        return coordinates.subList(index, index + 2);
     }
 }
