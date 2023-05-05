fbc37cf405955ad7a970c5666ff278b5af85d5eb
songsunkook <songsunkook@gmail.com>
2023-05-05 16:27:23 +0900

    feat: 삼각형 면적 구하기

--- src/main/java/service/TriangleCalculator.java
+++ src/main/java/service/TriangleCalculator.java
@@ -1,8 +1,9 @@
 package service;
 
-import domain.Coordinate;
-
+import java.util.Arrays;
 import java.util.List;
+
+import domain.Coordinate;
 
 public class TriangleCalculator extends CalculatorService {
     public TriangleCalculator(List<Coordinate> coordinates) {
@@ -11,7 +12,13 @@ public class TriangleCalculator extends CalculatorService {
 
     @Override
     public double calculate() {
-
-        return 0;
+        double[] len = new double[3];
+        for (int i = 0; i < 3; i++) {
+            List<Coordinate> coordinatesParam = coordinates.subList(0, 3);
+            LineCalculator lineCalculator = new LineCalculator(coordinatesParam);
+            len[i] = Math.abs(lineCalculator.calculate());
+        }
+        double s = (Arrays.stream(len).sum()) / 2;
+        return Math.sqrt(s * (s - len[0]) * (s - len[1]) * (s - len[2]));
     }
 }
