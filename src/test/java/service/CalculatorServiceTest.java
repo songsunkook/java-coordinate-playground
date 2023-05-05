24025b418fc362027a5b136426bed9775a1ef70a
songsunkook <songsunkook@gmail.com>
2023-05-05 19:05:27 +0900

    test: 삼각형 면적 계산 테스트 추가

--- src/test/java/service/CalculatorServiceTest.java
+++ src/test/java/service/CalculatorServiceTest.java
@@ -20,7 +20,15 @@ public class CalculatorServiceTest {
     @MethodSource("lineProvider")
     public void lineCalculatorTest(List<Coordinate> coordinates, double wantedResult) {
         CalculatorService calculatorService = new LineCalculator(coordinates);
-        assertEquals(calculatorService.calculate(), wantedResult, 0.000001);
+        assertEquals(calculatorService.calculate(), wantedResult, 0.001);
+    }
+
+    @ParameterizedTest
+    @DisplayName("정상적으로 삼각형 면적을 구하는가")
+    @MethodSource("triangleProvider")
+    public void triangleCalculatorTest(List<Coordinate> coordinates, double wantedResult) {
+        CalculatorService calculatorService = new TriangleCalculator(coordinates);
+        assertEquals(calculatorService.calculate(), wantedResult, 0.001);
     }
 
     @ParameterizedTest
@@ -28,7 +36,7 @@ public class CalculatorServiceTest {
     @MethodSource("rectangleProvider")
     public void rectangleCalculatorTest(List<Coordinate> coordinates, double wantedResult) {
         CalculatorService calculatorService = new RectangleCalculator(coordinates);
-        assertEquals(calculatorService.calculate(), wantedResult, 0.000001);
+        assertEquals(calculatorService.calculate(), wantedResult, 0.001);
     }
 
     private static Stream<Arguments> lineProvider() {
@@ -41,11 +49,22 @@ public class CalculatorServiceTest {
         ).map(arguments -> Arguments.of(arguments.get()[0], arguments.get()[1]));
     }
 
+    private static Stream<Arguments> triangleProvider() {
+        return Stream.of(
+            Arguments.of(Arrays.asList(new Coordinate(Arrays.asList(20, 1)), new Coordinate(Arrays.asList(20, 5)),
+                new Coordinate(Arrays.asList(10, 5))), 20),
+            Arguments.of(Arrays.asList(new Coordinate(Arrays.asList(10, 10)), new Coordinate(Arrays.asList(20, 15)),
+                new Coordinate(Arrays.asList(15, 15))), 12.5002),
+            Arguments.of(Arrays.asList(new Coordinate(Arrays.asList(5, 5)), new Coordinate(Arrays.asList(10, 10)),
+                new Coordinate(Arrays.asList(5, 10))), 12.5)
+        ).map(arguments -> Arguments.of(arguments.get()[0], arguments.get()[1]));
+    }
+
     private static Stream<Arguments> rectangleProvider() {
         return Stream.of(
             Arguments.of(Arrays.asList(new Coordinate(Arrays.asList(1, 2)), new Coordinate(Arrays.asList(1, 3)),
                 new Coordinate(Arrays.asList(3, 2)), new Coordinate(Arrays.asList(3, 3))
-                ), 2.0),
+            ), 2.0),
             Arguments.of(Arrays.asList(new Coordinate(Arrays.asList(10, 1)), new Coordinate(Arrays.asList(20, 11)),
                 new Coordinate(Arrays.asList(20, 1)), new Coordinate(Arrays.asList(10, 11))
             ), 100.0),
