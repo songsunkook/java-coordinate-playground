f307ccacb8fa20e3276ea12bf8f7fadc46765d93
songsunkook <songsunkook@gmail.com>
2023-05-13 15:31:23 +0900

    refactor: View에서 원시 데이터 가공 진행

--- src/main/java/domain/Coordinate.java
+++ src/main/java/domain/Coordinate.java
@@ -3,25 +3,14 @@ package domain;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
-
-import constant.ConstantNumber;
-import exception.NotCoordinateException;
-import exception.OutOfRangeException;
 
 public class Coordinate {
-    private static final int STRING_TRIM_INDEX = 1;
-
     private final int xPosition;
     private final int yPosition;
 
     public Coordinate(List<Integer> numbers) {
         xPosition = numbers.get(0);
         yPosition = numbers.get(1);
-    }
-
-    public Coordinate(String input) {
-        this(process(input));
     }
 
     @Override
@@ -80,28 +69,6 @@ public class Coordinate {
         return new Coordinate(Arrays.asList(minX, minY));
     }
 
-    private static List<Integer> process(String input) {
-        String original = input;
-        input = input.substring(STRING_TRIM_INDEX, input.length() - STRING_TRIM_INDEX);
-        List<String> list = Arrays.asList(input.split(","));
-        List<Integer> result = stringToIntList(list, original);
-        checkExceptions(result);
-        return result;
-    }
 
-    private static void checkExceptions(List<Integer> result) {
-        if (result.stream().anyMatch(i -> i < ConstantNumber.MIN_RANGE.getNumber() || i > ConstantNumber.MAX_RANGE.getNumber())) {
-            throw new OutOfRangeException(result);
-        }
-    }
 
-    private static List<Integer> stringToIntList(List<String> input, String original) {
-        try {
-            return input.stream()
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-        } catch (Exception e) {
-            throw new NotCoordinateException(original);
-        }
-    }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,9 +5,13 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
+import constant.ConstantNumber;
 import domain.Coordinate;
+import exception.NotCoordinateException;
+import exception.OutOfRangeException;
 
 public class InputView {
+    private static final int STRING_TRIM_INDEX = 1;
     private static final Scanner scanner = new Scanner(System.in);
     public static List<Coordinate> coordinates() {
         return separatingCoordinates(scanner.nextLine());
@@ -18,9 +22,35 @@ public class InputView {
             List<String> splitString = Arrays.asList(input.split("-"));
 
             return splitString.stream()
+                .map(InputView::process)
                 .map(Coordinate::new)
                 .collect(Collectors.toList());
         }
-        return List.of(new Coordinate(input));
+        return List.of(new Coordinate(process(input)));
+    }
+
+    private static List<Integer> process(String input) {
+        String original = input;
+        input = input.substring(STRING_TRIM_INDEX, input.length() - STRING_TRIM_INDEX);
+        List<String> list = Arrays.asList(input.split(","));
+        List<Integer> result = stringToIntList(list, original);
+        checkExceptions(result);
+        return result;
+    }
+
+    private static void checkExceptions(List<Integer> result) {
+        if (result.stream().anyMatch(i -> i < ConstantNumber.MIN_RANGE.getNumber() || i > ConstantNumber.MAX_RANGE.getNumber())) {
+            throw new OutOfRangeException(result);
+        }
+    }
+
+    private static List<Integer> stringToIntList(List<String> input, String original) {
+        try {
+            return input.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new NotCoordinateException(original);
+        }
     }
 }
